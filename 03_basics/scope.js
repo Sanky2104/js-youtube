a8b046cd56fb95a6a17cb8c6c558b3f7a835c35b
Sanky2104 <89095469+Sanky2104@users.noreply.github.com>
2025-08-31 10:48:22 +0000

    scope level

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -6,10 +6,47 @@ if (true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("inner:", a);
-    console.log(testGlobalScope);
+    // console.log("inner:", a);
+    // console.log(testGlobalScope);
 }
 
-console.log(a); //prints 300 as it should
-// console.log(b); //give error as it should
-console.log(c); //prints 30 instead of sans, thus shouldn't be used (as it doesnt follow scope)
+// console.log(a); //prints 300 as it should
+// // console.log(b); //give error as it should
+// console.log(c); //prints 30 instead of sans, thus shouldn't be used (as it doesnt follow scope)
+
+//////////////// Scope Level ///////////////////////
+
+// example to understand: chhote log badon se icecream maang sakte hain, lekin bade log chhote logon se icecream nahi maang sakte hain
+
+function one(){
+    const username = "sanskriti"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true){
+    const username = "sans"
+    if (username==="sans"){
+        const website = "youtube"
+        // console.log(username, website);
+    }
+    // console.log(website) //---> gives error
+}
+// console.log(username); //---> gives error
+
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+// addTwo(5) //gives error: Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2
+}
+// addTwo(5)
