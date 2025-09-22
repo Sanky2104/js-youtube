a3569677a1151eb9d7f784695145c7514d0d6541
Sanky2104 <89095469+Sanky2104@users.noreply.github.com>
2025-09-22 04:45:57 +0000

    Started Promise

---
+++ 09_advance_one/promise.js
@@ -0,0 +1,60 @@
+let promiseOne = new Promise(function(resolve, reject) {
+    // Do an async task like DB Calls, cryptography related tasks, network calls
+    setTimeout(function(){
+        console.log("Async task is complete")
+        resolve()
+    }, 1000)
+})
+
+// promiseOne.then directly corresponds to the resolve part of a promise
+
+promiseOne.then(function(){
+    console.log('promise consumed');
+})
+
+
+// Promise without storing it in a variable
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('async task 2');
+        resolve()
+    }, 1000)
+}).then(function(){
+  console.log('async task2 resolved');
+})
+
+let promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "sans", email:"sans@email.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+let promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let err = false
+        if (!err){
+            resolve({username:"sans", password:"12345"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+// chaining
+promiseFour
+.then((user) => {
+    console.log(user)
+    return user.username
+})
+.then((username) => {
+    console.log(username)
+})
+.catch(function(err){
+    console.log(err)
+})
+.finally(() => console.log("promise is either resolved or rejected"))
